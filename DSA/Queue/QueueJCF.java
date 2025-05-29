a5f8f9deeac8d382ef60186916f263b47fc3f5de
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-05-29 12:43:01 +0530

    queue

---
+++ DSA/Queue/QueueJCF.java
@@ -0,0 +1,22 @@
+package DSA.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueJCF {
+    public static void main(String args[]) {
+
+        //! Queue<Integer> q = new Queue<>();   X X WRONG
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        while (!q.isEmpty()) {
+            System.out.print(q.peek() + " ");
+            q.remove();
+        }
+    }
+}
